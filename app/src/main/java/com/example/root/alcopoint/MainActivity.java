256faad4b639deb17e710e494b86334c061807c4
mr.Robot <lontkovsky@gmail.com>
2017-09-05 17:29:09 +0300

    add some pictures and functionality to +- buttons

--- app/src/main/java/com/example/root/alcopoint/MainActivity.java
+++ app/src/main/java/com/example/root/alcopoint/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +32,57 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+
+
+
+
+
+    public void changePlus(View view){
+        int [] userImageList = new int[]{R.drawable.e, R.drawable.i, R.drawable.q, R.drawable.r, R.drawable.t, R.drawable.u,R.drawable.w, R.drawable.y};
+        Random random = new Random();
+        Integer rand = random.nextInt(userImageList.length - 1);
+        ImageView img1 = (ImageView) findViewById(R.id.section1);
+        ImageView img2 = (ImageView) findViewById(R.id.section2);
+        ImageView img3 = (ImageView) findViewById(R.id.section3);
+        ImageView img4 = (ImageView) findViewById(R.id.section4);
+        ImageView img5 = (ImageView) findViewById(R.id.section5);
+        ImageView img6 = (ImageView) findViewById(R.id.section6);
+        ImageView img7 = (ImageView) findViewById(R.id.section7);
+        ImageView img8 = (ImageView) findViewById(R.id.section8);
+        img1.setImageResource(userImageList[rand]);
+        img2.setImageResource(userImageList[rand]);
+        img3.setImageResource(userImageList[rand]);
+        img4.setImageResource(userImageList[rand]);
+        img5.setImageResource(userImageList[rand]);
+        img6.setImageResource(userImageList[rand]);
+        img7.setImageResource(userImageList[rand]);
+        img8.setImageResource(userImageList[rand]);
+
+    }
+
+    public void changeMinus(View view){
+        int [] userImageList = new int[]{R.drawable.e, R.drawable.i, R.drawable.q, R.drawable.r, R.drawable.t, R.drawable.u,R.drawable.w, R.drawable.y};
+        Random random = new Random();
+        Integer rand = random.nextInt(userImageList.length - 1);
+        ImageView img1 = (ImageView) findViewById(R.id.section1);
+        ImageView img2 = (ImageView) findViewById(R.id.section2);
+        ImageView img3 = (ImageView) findViewById(R.id.section3);
+        ImageView img4 = (ImageView) findViewById(R.id.section4);
+        ImageView img5 = (ImageView) findViewById(R.id.section5);
+        ImageView img6 = (ImageView) findViewById(R.id.section6);
+        ImageView img7 = (ImageView) findViewById(R.id.section7);
+        ImageView img8 = (ImageView) findViewById(R.id.section8);
+        img1.setImageResource(userImageList[rand-1]);
+        img2.setImageResource(userImageList[rand-1]);
+        img3.setImageResource(userImageList[rand-1]);
+        img4.setImageResource(userImageList[rand-1]);
+        img5.setImageResource(userImageList[rand-1]);
+        img6.setImageResource(userImageList[rand-1]);
+        img7.setImageResource(userImageList[rand-1]);
+        img8.setImageResource(userImageList[rand-1]);
+
     }
 
 }
